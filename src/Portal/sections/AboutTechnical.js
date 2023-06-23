7246bba0df8d121a5ebaf2a88528ca8d052c2992
zveinn <info@sveinn.is>
2023-06-23 19:28:26 +0000

    fixing text

--- src/Portal/sections/AboutTechnical.js
+++ src/Portal/sections/AboutTechnical.js
@@ -15,7 +15,7 @@ const AboutTechnical = (props) => {
       </div>
       <div className="subtitle font-big-section-description">
 
-        Niceland VPN could be considered a mix between TOR and a VPN. At its core it is a global mesh-network of routers that have VPN endpoints. Niceland operates on Layer 3, more specifically it forwards TCP and UDP traffic to the designated VPN endpoint. The reason it operates on Layer 3 and not Layer 2 is for security and performance reasons. Layer 2 VPNs tend to forward more network packets then needed for connectivity and enables a number of malicious.
+        Niceland VPN could be considered a mix between TOR and a VPN. At its core it is a global mesh-network of routers that have VPN endpoints. Niceland operates on Layer 3, more specifically it forwards TCP and UDP traffic to the designated VPN endpoint. The reason it operates on Layer 3 and not Layer 2 is for security and performance reasons. Layer 2 VPNs tend to forward more network packets then needed for connectivity and enables a number of malicious application to function within the VPN network.
         <br></br>
         <br></br>
         Niceland is programmed from the ground up using golang, and it was designed at its core to not function like normal VPNs. It does not rely on the traditional way of Packet Forwarding but instead relies on user-specific TCP tunnels and packet manipulation to forward network packets. This increases the isolation of each user network flow within the system and prevents malicious actors from abusing packet forwarding.
