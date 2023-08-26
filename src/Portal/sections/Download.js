a709fa0cca114be986000a5deb7926dd990363ab
sondreal <sondre@villdyr.no>
2023-08-26 17:01:26 +0000

    made the download boxes clickable

--- src/Portal/sections/Download.js
+++ src/Portal/sections/Download.js
@@ -1,6 +1,18 @@
 import React, { useEffect, useState, useId } from "react";
 import { useNavigate, Navigate, Link } from "react-router-dom";
 
+function handleWindowsClick() {
+  window.open('https://github.com/tunnels-is/nicelandvpn-desktop/releases/download/v1.1.3/NicelandVPN-Windows.exe', '_blank');
+}
+
+function handleMacClick() {
+  window.open('https://github.com/tunnels-is/nicelandvpn-desktop/releases/download/v1.1.3/NicelandVPN-MacOS.zip', '_blank');
+}
+
+function handleLinuxClick() {
+  window.open('https://github.com/tunnels-is/nicelandvpn-desktop/releases/latest', '_blank');
+}
+
 const Download = (props) => {
 
   const navigate = useNavigate();
@@ -11,6 +23,7 @@ const Download = (props) => {
       <div className="download-title font-section-title">
         Available Downloads
       </div>
+      
       <div className="subtitle font-section-subtitle">
 
         <a target="_blank" href="https://github.com/tunnels-is/nicelandVPN/releases/latest">
@@ -18,33 +31,24 @@ const Download = (props) => {
         </a>
 
       </div>
-
-      <div className="s1 box box-shadow">
-
+    
+      <div className="s1 box box-shadow" onClick={handleWindowsClick} style={{cursor: 'pointer'}}>
         <img className="icon" src="https://raw.githubusercontent.com/tunnels-is/media/master/nl-website/v2/temp-win11.svg"></img>
         <div className="title">
-
-          <a target="_blank" href="https://github.com/tunnels-is/nicelandvpn-desktop/releases/download/v1.1.3/NicelandVPN-Windows.exe">
             Download for Windows
-          </a>
         </div>
       </div>
-
-      <div className="s2 box box-shadow">
+      <div className="s2 box box-shadow" onClick={handleMacClick} style={{cursor: 'pointer'}}>
         <img className="icon" src="https://raw.githubusercontent.com/tunnels-is/media/master/nl-website/v2/mac.svg"></img>
         <div className="title">
-          <a target="_blank" href="https://github.com/tunnels-is/nicelandvpn-desktop/releases/download/v1.1.3/NicelandVPN-MacOS.zip">
             Download for MacOS
-          </a>
         </div>
       </div>
 
-      <div className="s3 box box-shadow">
+      <div className="s3 box box-shadow" onClick={handleLinuxClick} style={{cursor: 'pointer'}}>
         <img className="icon" src="https://raw.githubusercontent.com/tunnels-is/media/master/nl-website/v2/temp-linux.png"></img>
-        <div className="title">
-          <a target="_blank" href="https://github.com/tunnels-is/nicelandvpn-desktop/releases/latest">
+        <div className="title">      
             Download for Linux
-          </a>
         </div>
       </div>
 
