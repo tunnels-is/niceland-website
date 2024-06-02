0306769cb5e8940e20054f2cf72dda1566b63dca
zveinn <info@sveinn.is>
2024-06-01 20:00:10 -0700

    v2.0.3

--- src/Portal/sections/Download.jsx
+++ src/Portal/sections/Download.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Download = (props) => {
 
 	function handleWindowsClick() {
-		window.open("https://github.com/tunnels-is/NicelandVPN/releases/download/v2.0.2/NicelandVPN-Windows.exe", '_blank');
+		window.open("https://github.com/tunnels-is/NicelandVPN/releases/download/v2.0.3/NicelandVPN-Windows-app.exe", '_blank');
 	}
 
 	function handleMacClick() {
-		window.open('https://github.com/tunnels-is/NicelandVPN/releases/download/v2.0.2/NicelandVPN-MacOS.zip', '_blank');
+		window.open('https://github.com/tunnels-is/NicelandVPN/releases/download/v2.0.3/NicelandVPN-MacOS-app.zip', '_blank');
 		// alert("soon!")
 	}
 
